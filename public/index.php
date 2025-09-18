0c1787cc842dde03be0f43adc4c157309c044184
Brayan Hernandez <bhrestrepo_27@hotmail.com>
2025-09-18 13:01:13 -0500

    Update files

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 
 // Habilitar errores en desarrollo (puedes quitar esto en producción)
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
 require_once __DIR__ . '/../app/config/config.php';
 require_once __DIR__ . '/../app/routes.php';
